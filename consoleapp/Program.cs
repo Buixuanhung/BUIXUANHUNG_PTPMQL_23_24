84b821ffa877f2dcf1fb7e640913494011ad7a22
Buixuanhung <buixuanhung16042001@gmail.com>
2023-08-16 11:25:38 +0700

    do- while

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -3,11 +3,11 @@
     private static void Main(string[] args )
     {
         int a = 1;
-        while( a< 10)
-        {
-            System.Console.WriteLine("vong lap thu {0}, a");
+        do{
+            System.Console.WriteLine("vong lap thu {0},a");
             a++;
-        }
+        }while(a<10);
+        
     }
         
     
