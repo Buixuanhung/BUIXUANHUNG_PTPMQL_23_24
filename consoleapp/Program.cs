8a0a5eced34c44fd3b555a880833f482ddbf4069
Buixuanhung <buixuanhung16042001@gmail.com>
2023-08-16 10:34:28 +0700

    cac toan tu

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -2,10 +2,11 @@
 {
     private static void Main(string[] args )
     {
-        string str = "123";
-        int ketqua;
-        bool kiemtra = false;
-        kiemtra = int.TryParse(str,out(ketqua));
-        System.Console.WriteLine("ketqua = " + ketqua);
+        int a,b;
+        System.Console.Write("a = ");
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("b = ");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
     }
 }
